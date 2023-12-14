6c1ad29e2b346a3503c0857b88ed72c27a328953
USER <USER@LAPTOP-WORLD>
2023-12-14 16:09:43 +0100

    Ajoutez des fichiers projet.

---
+++ Source/TEST_GESTION_ET_SUPERVISION_DU_RESTAURANT/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TEST_GESTION_ET_SUPERVISION_DU_RESTAURANT")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("TEST_GESTION_ET_SUPERVISION_DU_RESTAURANT")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b397ebad-5dd8-4cef-8d09-50c06c999f9c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Source/TEST_GESTION_ET_SUPERVISION_DU_RESTAURANT/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace TEST_GESTION_ET_SUPERVISION_DU_RESTAURANT
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
