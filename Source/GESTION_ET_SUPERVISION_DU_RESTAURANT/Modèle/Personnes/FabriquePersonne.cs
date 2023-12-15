90e208d0a6b9841299e9430c1b7a16374f1f819f
Sam Onema <Sam Onema@SAMUEL-ONEMA>
2023-12-15 17:31:28 +0100

    Test PersonneFabrique

---
+++ Source/GESTION_ET_SUPERVISION_DU_RESTAURANT/Modèle/Personnes/FabriquePersonne.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GESTION_ET_SUPERVISION_DU_RESTAURANT.Modèle.Personnes.Salle;
+
+
+namespace GESTION_ET_SUPERVISION_DU_RESTAURANT.Modèle.Personnes
+{
+    public class FabriquePersonne
+    {
+        public AEmploye CreateEmploye(Roles role)
+        {
+            AEmploye employe = null;
+            
+            
+
+            return employe;
+        }
+
+        public Client CreateClient(Caractere caractere)
+        {
+            Client client;
+            
+            return null;
+        }
+
+    }
+}
--- Source/TEST_GESTION_ET_SUPERVISION_DU_RESTAURANT/TestPersonnesFrabrique.cs
+++ Source/TEST_GESTION_ET_SUPERVISION_DU_RESTAURANT/TestPersonnesFrabrique.cs
@@ -1,15 +1,36 @@
 ﻿
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Xunit.Sdk;
+using GESTION_ET_SUPERVISION_DU_RESTAURANT.Modèle.Personnes;
+using GESTION_ET_SUPERVISION_DU_RESTAURANT.Modèle.Personnes.Salle;
 
 namespace TEST_GESTION_ET_SUPERVISION_DU_RESTAURANT
 {
     [TestClass]
     public class TestPersonnesFrabrique
     {
-        [TestMethod]
-        public void TestMethod1()
+        private FabriquePersonne _fabrique;
+
+        public TestPersonnesFabrique()
         {
+            _fabrique = new FabriquePersonne();
+            Assert.IsNotNull(_fabrique);
+        }
+
+
+        [TestMethod]
+        public void PersonneFactory()
+        {
+            //Object[] paramCli = { "Test", Caractere.Lent, Gouts.Salé, true,  };
+            //Object[] paramEmp = { "Jacque" };
+            var VchefDeCuisine = Roles.ChefDeCuisine;
+            var Vclient = Caractere.Lent;
+
+            var newPersonne = _fabrique.CreateClient(Vclient);
+            var newEmploye = _fabrique.CreateEmploye(VchefDeCuisine);
+
+            Assert.AreEqual(newEmploye.Role, VchefDeCuisine);
+            Assert.AreEqual(newPersonne.Caractere, Vclient);
         }
     }
 }
