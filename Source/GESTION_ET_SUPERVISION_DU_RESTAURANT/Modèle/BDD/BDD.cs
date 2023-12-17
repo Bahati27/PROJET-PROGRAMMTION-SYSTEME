9b7960acbd2461faea4f0510bccba0bcd0afac9c
Eliott HOLEL <eliottholel@gmail.com>
2023-12-17 14:28:01 +0100

    Ajout des classes BDD, et  TestBDD.

---
+++ Source/GESTION_ET_SUPERVISION_DU_RESTAURANT/Modèle/BDD/BDD.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GESTION_ET_SUPERVISION_DU_RESTAURANT.Modèle.BDD
+{
+    public class BDD
+    {
+        public List<string> getRecette(string categorie)
+        {
+            return  null;
+        }
+
+        public int getPrice(string recette)
+        {
+            return 0;
+        }
+
+        public List<string> getEtape(string Recette) 
+        { 
+            return null; 
+        }
+    }
+}
--- Source/TEST_GESTION_ET_SUPERVISION_DU_RESTAURANT/TestBDD.cs
+++ Source/TEST_GESTION_ET_SUPERVISION_DU_RESTAURANT/TestBDD.cs
@@ -7,8 +7,49 @@ namespace TEST_GESTION_ET_SUPERVISION_DU_RESTAURANT
     public class TestBDD
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestGetRecette()
         {
+            // Arrange
+            string categorie = "Plat";
+            List<string> recettesAttendues = new List<string> { "Burger Vegan", "Steak Frites" };
+
+            // Act
+            List<string> recettes = getRecette(categorie);
+
+            // Assert
+            CollectionAssert.AreEqual(recettesAttendues, recettes);
+        }
+
+        [TestMethod]
+        public void TestGetPrice()
+        {
+            string nomRecettte = "Burger Vegan";
+            int prixAttendu = 26;
+
+            int prix = getPrice(nomRecettte);
+
+            Assert.AreEqual(prixAttendu, prix);
+        }
+
+        [TestMethod]
+        public void TestGetEtape()
+        {
+            string nomRecettte = "Burger Vegan";
+            List<string> etapesAttendues = new List<string> { "Cuire", "Preparation" };
+
+            List<string> etapes = getEtape(nomRecettte);
+
+            CollectionAssert.AreEqual(etapesAttendues, etapes);
+        }
+
+        [TestMethod]
+        public void TestGetRecette_CategorieInexistante()
+        {
+            string categorie = "Sortie";
+
+            List<string> recettes = getRecette(categorie);
+
+            Assert.AreEqual(0, recettes.Count);
         }
     }
 }
