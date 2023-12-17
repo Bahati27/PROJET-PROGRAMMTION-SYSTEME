7ebf81ed0ca24371c1041c58e8fbb2baa1ec1621
Eliott HOLEL <eliottholel@gmail.com>
2023-12-17 18:56:37 +0100

    Ajout de la classe stockManager

--- Source/GESTION_ET_SUPERVISION_DU_RESTAURANT/Modèle/BDD/BDDControleur.cs
+++ Source/GESTION_ET_SUPERVISION_DU_RESTAURANT/Modèle/BDD/BDDControleur.cs
@@ -8,6 +8,41 @@ namespace GESTION_ET_SUPERVISION_DU_RESTAURANT.Modèle.BDD
 {
     class BDDControleur
     {
+        IBDD Bdd = new BDD();
 
+        public void getReservation()
+        {
+            Bdd.getReservation();
+        }
+
+        public void setReservation(string Nom, int nbPersonne, int Heure)
+        {
+            Bdd.setReservation(Nom, nbPersonne, Heure);
+        }
+
+        public void updateStock(string recette, int quantite = 1)
+        {
+            Bdd.updateStock(recette, quantite);
+        }
+
+        public List<string> getRecette(string categorie)
+        {
+            return Bdd.getRecette(categorie);
+        }
+
+        public void reStock()
+        {
+            Bdd.reStock();
+        }
+
+        public List<string> getEtape(string recette)
+        {
+            return Bdd.getEtape(recette);
+        }
+
+        public int getPrix(string recette)
+        {
+            return Bdd.getPrice(recette);
+        }
     }
 }
