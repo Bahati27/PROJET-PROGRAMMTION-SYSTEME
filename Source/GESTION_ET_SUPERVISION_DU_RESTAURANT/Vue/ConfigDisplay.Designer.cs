e821bd4a08cfe3746c5022f18f1421743072f001
Bahati27 <116849244+Bahati27@users.noreply.github.com>
2024-01-03 18:48:24 +0100

    Christine Diango

--- Source/GESTION_ET_SUPERVISION_DU_RESTAURANT/Vue/ConfigDisplay.Designer.cs
+++ Source/GESTION_ET_SUPERVISION_DU_RESTAURANT/Vue/ConfigDisplay.Designer.cs
@@ -28,13 +28,39 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btn_Save = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_Save
+            // 
+            this.btn_Save.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_Save.Location = new System.Drawing.Point(12, 206);
+            this.btn_Save.Name = "btn_Save";
+            this.btn_Save.Size = new System.Drawing.Size(372, 50);
+            this.btn_Save.TabIndex = 0;
+            this.btn_Save.Text = "Sauvegarder";
+            this.btn_Save.UseVisualStyleBackColor = true;
+            this.btn_Save.Click += new System.EventHandler(this.btn_Save_Click);
+            // 
+            // ConfigDisplay
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(396, 268);
+            this.Controls.Add(this.btn_Save);
+            this.Name = "ConfigDisplay";
+            this.ShowIcon = false;
+            this.Text = "Configuration";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.ConfigDisplay_Closed);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
-        private System.Windows.Forms.Button btn_save;
+
+        private System.Windows.Forms.Button btn_Save;
     }
 }
